131fa2e224a00cf7222c87f599d4e00d174fa6dc
Cameron Ehrlich <cameronehrlich@gmail.com>
2013-05-25 17:47:10 -0700

    Initial commit.

---
+++ ReliefSquad-App/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReliefSquad::Application.config.secret_token = '6c1e9ea771be0d606cd153f1aa353289281c4aa45a01f5779122297dee108c52eba63bc90445f2606670343fc4f89dea7ebee96a959c9a4830816a71a4759361'
